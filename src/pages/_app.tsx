fccd30063c4031f02eca20ee40754134fce4c7a2
Savitur Maharaj <saviturmaharaj@gmail.com>
2023-11-11 15:02:02 -0700

    Setup Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import { LoadingContextProvider } from '@/contexts/loadingContext'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '@/components/Layout'
+import { SessionProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <LoadingContextProvider><Component {...pageProps} /></LoadingContextProvider>
+export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+  return 
+    <SessionProvider>
+      <LoadingContextProvider> <Layout><Component {...pageProps} /></Layout></LoadingContextProvider>
+    </SessionProvider>
 }
+
