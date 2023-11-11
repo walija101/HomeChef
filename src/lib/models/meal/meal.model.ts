f999afc99542b5d8e3f53c581aa6423a0a503f6d
Yotam <Yotam1802@gmail.com>
2023-11-11 14:33:27 -0700

    Branch

--- src/lib/models/meal/meal.model.ts
+++ src/lib/models/meal/meal.model.ts
@@ -1,7 +1,10 @@
 import { Schema, model, models } from 'mongoose';
 
 const MealSchema = new Schema({
-    
+    name: {
+        type: String,
+        required: true
+    }
 }, { collection: 'Meals', timestamps: true });
 
 const Meal = models.Meal || model('User', MealSchema);
