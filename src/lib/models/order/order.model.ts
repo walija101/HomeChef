b790f47f84e25392e47c6407740250a149c1a8d9
Yotam <Yotam1802@gmail.com>
2023-11-11 19:04:41 -0700

    commit

--- src/lib/models/order/order.model.ts
+++ src/lib/models/order/order.model.ts
@@ -20,14 +20,44 @@ const OrderSchema = new Schema({
 const Order = models.Order || model('Order', OrderSchema);
 
 OrderSchema.statics.createOrder = async function(data) {
-    const {meal, user} = data
+    const {meal, user, status} = data
     const order: { 
-        meal: string, user: string 
+        meal: string, user: string, status: string 
     } = {
         meal: meal,
-        user: user
+        user: user,
+        status: status
     }
     return (await this.create(order));
+}
+
+OrderSchema.statics.updateOrder = async function (orderId, data) {
+    const theOrder = await this.findById(orderId);
+    if(!theOrder)
+        throw Error("Could not find order");
+
+    let updateOrder: {
+        status?: string,
+        updatedAt?: string
+    } = {};
+    Object.keys(data).forEach(key => {
+        if(key.trim() === "")
+            throw Error(`Field name can't be empty`);
+        else {
+            let fieldName = key.trim().toLowerCase() as ('status');
+            let newValue = typeof data[key] === 'string' ? data[key].trim() : data[key];
+
+            if(fieldName === 'status')
+                updateOrder.status = newValue;
+            else
+                console.log(`Cannot update field "${fieldName}"`);
+        }
+    });
+
+    if(Object.keys(updateOrder).length > 0) {
+        updateOrder.updatedAt = new Date().toISOString().slice(0, 23)+"+00:00";
+        await this.findOneAndUpdate({ _id: orderId }, updateOrder);
+    }
 }
 
 export default Order;
