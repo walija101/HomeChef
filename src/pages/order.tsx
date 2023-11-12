4a95bfd4da398148c643ddc9b83030cd38ce3508
Savitur Maharaj <saviturmaharaj@gmail.com>
2023-11-12 00:56:04 -0700

    Add Order Page

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,21 +1,79 @@
 import styles from "@/styles/Order.module.scss";
+import { useState } from "react";
 import { ClassNames } from "@emotion/react";
 
 export default function Order() {
+  const [mode, setMode] = useState({
+    type: "pickup",
+    address: ""
+  });
+
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>){
+    const {name, value} = event.target
+    setMode(prev => {
+        return{
+            ...prev,
+            [name]: value
+        }
+    })
+}
+
   return (
     <div className={styles.container}>
         <h1>ORDER CONFIRMATION</h1>
         <h2>Meal name: <span>Joe's Casserole</span></h2>
         <h2>Price: <span>$20.00</span></h2>
         <h2>Description: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nulla id, a culpa in repellendus voluptas aliquid.</span></h2>
-        <div className={styles.button}>
-          <button>Confirm Order</button>
+        <h2>Ingredients:</h2>
+        <div className={styles.ingredients}>
+          <ul>
+            <li>Cassava</li>
+            <li>Chicken</li>
+            <li>Pasta</li>
+          </ul>
         </div>
-
         <div className={styles.orderOptions}>
           <form>
-            
+          <legend>How do you plan to get your yummy food?</legend>
+                    <input 
+                        type="radio"
+                        id="delivery"
+                        name="type"
+                        value="delivery"
+                        checked={mode.type === "delivery"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="customer">Delivery (extra charge)</label>
+                    <br />
+
+                    <div className= {styles.address}>
+                      {mode.type==="delivery" ? (
+                        <input
+                          type="text"
+                          placeholder="Address"
+                          onChange={handleChange}
+                          name="address"
+                          value={mode.address}
+                          />
+                      ) : null}
+                    </div>
+
+                    <input 
+                        type="radio"
+                        id="pickup"
+                        name="type"
+                        value="pickup"
+                        checked={mode.type === "pickup"}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="chef">Pickup</label>
+                    <br />
           </form>
+
+          <div className={styles.button}>
+            <button>Confirm Order</button>
+          </div>
         </div>
     </div>
   )
