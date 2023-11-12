99cb928550fd8a5705b4a76250f881865a391632
Jasmit <jasmit2294@protonmail.com>
2023-11-11 18:25:55 -0700

    UI Changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "@/styles/Footer.module.scss";
 function Footer() {
   return (
     <div className={styles.footer}>
-      <h3>Home Chef 2023</h3>
+      <h3>HomeChef 2023</h3>
       <div>Reducing Food Waste One Meal at a time!</div>
     </div>
   );
