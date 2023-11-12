cabedc8540ace49a10a358ff713214f1455353f6
JamesRobert20 <jmtendamema@gmail.com>
2023-11-12 00:30:26 -0700

    Added chef section

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -5,17 +5,26 @@ type MealCardProps = {
     meal: MealType
 }
 export default function MealCard({ meal }: MealCardProps) {
-  return (
-    <div className={styles.card}>
-        <div className={styles.pictureContainer}>
-            <img src="./images/food.jpeg" style={{ width: '100%', height: '100%' }}/>
+    return (
+        <div className={styles.card}>
+            <div className={styles.pictureContainer}>
+                <img src="./images/food.jpeg" style={{ width: '100%', height: '100%' }} />
+            </div>
+            <div className={styles.nameSection}>
+                <h1 className={styles.foodName}>{meal.name}</h1>
+                <h2 className={styles.price}>{`$${meal.price}`}</h2>
+            </div>
+            <p>{meal.description}</p>
+            <p>{`Ingredients include: ${meal.ingredients.join(', ')}`}</p>
+            <div className={styles.chefSection}>
+                <div className={styles.chefIcon}>
+                    <img src='./images/chef.jpg' />
+                </div>
+                <div className={styles.chefInfoSection}>
+                    <h2 className={styles.chefName}>{meal.chef.toUpperCase()}</h2>
+                    <p>Top Rated Cook</p>
+                </div>
+            </div>
         </div>
-        <div className={styles.nameSection}>
-            <h2 className={styles.foodName}>{meal.name}</h2>
-            <h1 className={styles.price}>{`$${meal.price}`}</h1>
-        </div>
-        <p>{meal.description}</p>
-        <p>{`Ingredients include: ${meal.ingredients.join(', ')}`}</p>
-    </div>
-  )
+    )
 }
